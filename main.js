06bec6c0dcd4528dca4487c0970ea2d6e99a581c
Bindiya <bindiya.kejdiwal@gmail.com>
2023-01-18 14:13:18 +0000

    Adding file

---
+++ main.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+let textFileDataEncode = fs.readFileSync("test.txt", { encoding: "utf-8" });
+
+/* Another way to read text file
+// let textFileData = fs.readFileSync("test.txt");
+// console.log(textFileData.toString());
+console.log(textFileDataEncode); */
+
+const emailAddRegExp = /(@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g;
+const domainRegExp = /@softwire\.com(?= )/gi;
+
+console.log(textFileDataEncode.match(domainRegExp).length);
+
+let emailAddressArray = textFileDataEncode.match(emailAddRegExp);
+console.log(emailAddressArray);
+const domainMap = new Map();
+let domainCounter = 0;
+
+for (let i = 0; i < emailAddressArray.length; i++) {
+    for (let j = 1; j < emailAddressArray.length; j++) {
+        if (emailAddressArray(i) === emailAddressArray(j)) {
+            domainCounter++;
+        }
+
+
+
+    }
+    domainMap.set(emailAddressArray(i), domainCounter);
+}
+
+console.log(domainMap);
+
+// // /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/;
+// var emailAddress = [];
+
+// emailAddress = textFileData.match(emailAddRegExp);
+// console.log(emailAddress);
+
+
